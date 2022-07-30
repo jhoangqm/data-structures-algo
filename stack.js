c9bf426306e4dec85416405b919c85bee34f1789
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-30 19:43:06 -0400

    Adds basic implementation of stack data structure

---
+++ stack.js
@@ -0,0 +1,19 @@
+function execute(program) {
+    // initialize the stack
+    const stack = [];
+    for (const instruction of program) {
+        if (instruction == "peek") {
+            // print out the top item in stack
+            console.log(stack[stack.length - 1]);
+        } else if (instruction == "pop") {
+            // pop the top item in stack
+            stack.pop();
+        } else {
+            // get the data in the "push" instruction
+            const data = parseInt(instruction.substring(5));
+            // push data to the top of stack
+            stack.push(data);
+        }
+    }
+    return stack;
+}
