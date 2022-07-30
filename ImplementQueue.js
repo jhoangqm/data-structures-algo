1b3b5ecda33a1ce259fe85709eb809a0375a9a98
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-30 19:42:29 -0400

    Adds Basic implementation of queue data structure

---
+++ ImplementQueue.js
@@ -0,0 +1,29 @@
+function execute(program) {
+    // initialize the queue
+    const queue = [];
+    for (const instruction of program) {
+        if (instruction == "peek") {
+            if (queue.length === 0) {
+                // print warning message if queue is empty
+                console.log("Queue is empty!");
+            } else {
+                // print out the first item in queue
+                console.log(queue[0]);
+            }
+        } else if (instruction == "pop") {
+            if (queue.length === 0) {
+                // print warning message if queue is empty
+                console.log("Queue is empty!");
+            } else {
+                // pop the first item in queue
+                queue.shift();
+            } 
+        } else {
+            // get the data in the "push" instruction
+            const data = parseInt(instruction.substring(5));
+            // push data to the end of queue
+            queue.push(data);
+        }
+    }
+    return queue;
+}
