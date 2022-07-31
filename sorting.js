c3417872ae0238deeaf5b8ba59691ad701307453
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-31 14:58:40 -0400

    Adds time complexity for the sorting algorithms

--- sorting.js
+++ sorting.js
@@ -1,5 +1,9 @@
+// For each n item in the list, the time complexity to insert it into the sorted list is O(i),
+// where i is the index of that item. Overall, the time complexity is **O(n * (n - 1) / 2)**, 
+// which is equivalent to **O(n^2)**
+
 // creating the sort method with vanilla javascript
-function sortList(unsortedList) {
+function sortListInsertion(unsortedList) {
     for (let i = 0; i < unsortedList.length; i++) {
         let current = i;
         while (current > 0 && unsortedList[current] < unsortedList[current - 1]) {
@@ -12,11 +16,17 @@ function sortList(unsortedList) {
     return unsortedList;
 }
 
+
 // sort method implemented in JavaScript
 function sortListFunction(unsortedList) {
     return unsortedList.sort(( a , b ) => a - b)
 }
 
+/*For each n item in the list,
+the time complexity to find the smallest item in the unsorted pile is O(n - i), 
+where i is the index of that item. 
+Overall, the time complexity is **O(n * (n + 1) / 2)**, which is equivalent to **O(n^2)**.
+ */
 //  find the smallest item from the unsorted pile and add it to the sorted pile.
 function sortListSelection(unsortedList) {
     const n = unsortedList.length;
@@ -34,6 +44,7 @@ function sortListSelection(unsortedList) {
     return unsortedList;
 }
 
+// The time complexity of this algorithm, like before, is **O(n^2)**, because it is essentially two loops.
 // for each pass, we use a pointer to point at the first element of the list. For each cycle, we compare it to the next element in the list and swap them if the current item is greater, then move the pointer by one until it reaches the end of the list. We repeat this process until the list becomes sorted. The list is sorted if, during a pass, no swapping occurs.
 function sortListBubble(unsortedList) {
     const n = unsortedList.length;
@@ -52,7 +63,7 @@ function sortListBubble(unsortedList) {
     return unsortedList;
 }
 
-console.log(sortList([1,6,2,3,7,10]))
+console.log(sortListInsertion([1,6,2,3,7,10]))
 console.log(sortListFunction([1,5,2,3,7,10]))
 console.log(sortListSelection([1,5,2,3,7,10]))
 console.log(sortListBubble([1,5,2,3,7,10]))
