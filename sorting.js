2359d7f45c95f10bc6970329e69d9509430d7fd3
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-31 14:49:07 -0400

    Adds more to the sorting list algo

--- sorting.js
+++ sorting.js
@@ -13,10 +13,27 @@ function sortList(unsortedList) {
 }
 
 // sort method implemented in JavaScript
-
 function sortListFunction(unsortedList) {
     return unsortedList.sort(( a , b ) => a - b)
 }
 
+//  find the smallest item from the unsorted pile and add it to the sorted pile.
+function sortListSelection(unsortedList) {
+    const n = unsortedList.length;
+    for (var i = 0; i < n; i++) {
+        let minIndex = i;
+        for (var j = i; j < n; j++) {
+            if (unsortedList[j] < unsortedList[minIndex]) {
+                minIndex = j;
+            }
+        }
+        const temp = unsortedList[i];
+        unsortedList[i] = unsortedList[minIndex];
+        unsortedList[minIndex] = temp;
+    }
+    return unsortedList;
+}
+
 console.log(sortList([1,6,2,3,7,10]))
 console.log(sortListFunction([1,5,2,3,7,10]))
+console.log(sortListSelection([1,5,2,3,7,10]))
